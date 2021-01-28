a20c5d118ce3e6a5195804a9ee56c40be7e4aacd
RaffaeleFiorillo <raffadeus8@gmail.com>
2021-01-28 19:01:08 -0100

    update

--- gameOver.py
+++ gameOver.py
@@ -1,6 +1,6 @@
-
 import pygame
 from pygame.locals import *
+
 
 class GameOver(object):
     def __init__(self, screen):
@@ -9,109 +9,95 @@ class GameOver(object):
         self.menuControl = 200
         self.font = pygame.font.SysFont("comicsansms", 120)
         self.font.set_bold(True)
-        self.play = [pygame.image.load(f"Imagens/buttons/play"+str(i)+".png") for i in range (2)]
+        self.play = [pygame.image.load(f"Imagens/buttons/play"+str(i)+".png") for i in range(2)]
         self.playState = self.play[1]
-        self.mainMenu = [pygame.image.load(f"Imagens/buttons/mainMenu"+str(i)+".png") for i in range (2)]
+        self.mainMenu = [pygame.image.load(f"Imagens/buttons/mainMenu"+str(i)+".png") for i in range(2)]
         self.mainMenuState = self.mainMenu[0]
+        self.text = []
         self.savingBestsScores()
-        self.save = False
+        self.back = None
         self.run = True
         self.allPosition = [(280, 350), (280, 400)]
-
-
 
     def settingGameOverMenu(self):
         self.back = pygame.image.load("Imagens/buttons/gameOver.png").convert_alpha()
         self.screen.blit(self.back, (0, 0))
 
         # Controling menu buttons efects
-        if (self.menuControl == 200):
+        if self.menuControl == 200:
             self.playState = self.play[1]
             self.mainMenuState = self.mainMenu[0]
-        elif(self.menuControl == 300):
+        elif self.menuControl == 300:
             self.playState = self.play[0]
             self.mainMenuState = self.mainMenu[1]
 
         self.font = pygame.font.SysFont("comicsansms", 120)
-        size = pygame.font.Font.size(self.font,'Game Over')
-        line = self.font.render('Game Over', True, (120, 0,0))
+        size = pygame.font.Font.size(self.font, 'Game Over')
+        line = self.font.render('Game Over', True, (120, 0, 0))
         self.screen.blit(line, ((720/2-size[0]/2), 20))
-
         self.font = pygame.font.SysFont("comicsansms", 50)
-
         y=270
         for text in self.text:
-            size = pygame.font.Font.size(self.font,text)
-            line = self.font.render(text, True, (255, 157,53))
+            line = self.font.render(text, True, (255, 157, 53))
             self.screen.blit(line, (20, y))
             y += 30
-
         self.screen.blit(self.playState, self.allPosition[0])
         self.screen.blit(self.mainMenuState, self.allPosition[1])
-
 
     def drawGameOverMenu(self):
         while self.run:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     return False
-
             pressed_keys = pygame.key.get_pressed()
-            if(pressed_keys[K_DOWN]):
+            if pressed_keys[K_DOWN]:
                 pygame.time.delay(200)
-                if(self.menuControl == 300):
+                if self.menuControl == 300:
                     self.menuControl = 200
                 else:
                     self.menuControl += 100
-            elif(pressed_keys[K_UP]):
+            elif pressed_keys[K_UP]:
                 pygame.time.delay(200)
-                if(self.menuControl == 200):
+                if self.menuControl == 200:
                     self.menuControl = 200
                 else:
                     self.menuControl -= 100
-
-            if((pressed_keys[K_RETURN])and(self.menuControl==200)):
+            if pressed_keys[K_RETURN] and self.menuControl==200:
                 pygame.time.delay(200)
                 return 'game'
-            elif((pressed_keys[K_RETURN])and(self.menuControl==300)):
+            elif pressed_keys[K_RETURN] and self.menuControl==300:
                 pygame.time.delay(200)
-                return 'exit_menu'
-
-            if self.save:
-                self.savingBestsScores()
-
+                return 'main_menu'
             self.settingGameOverMenu()
             pygame.display.update()
         return True
 
-
     def savingBestsScores(self):
-
-        text1 = 'Score: '
-        text2 = 'Basket: '
+        print(1)
         # data last game
-        file = open('save/resultado_partida.txt','r')
+        file = open('save/resultado_partida.txt', 'r')
         data = file.read().split(" ")
         file.close()
         score = int(data[0])
         sweets = int(data[1])
-
         # Data Best score ever
-        file = open('save/data.txt','r')
+        file = open('save/data.txt', 'r')
         data = file.read().split(' ')
         file.close()
         currentBestScore = int(data[0])
         currentBestSweets = int(data[1])
 
-        if(score > currentBestScore):
+        if score > currentBestScore:
             text1 = 'New best Score: '
             currentBestScore = score
             text2 = 'Basket: '
             currentBestSweets = sweets
+            file = open("save/data.txt", "w")
+            file.write(f"{currentBestScore} {currentBestSweets}")
+            file.close()
         else:
             currentBestScore = score
-
-        #score
-        self.text = [text1+str(currentBestScore),text2+str(currentBestSweets)]
-
-        self.save = False
+            text1 = 'Score: '
+            text2 = 'Basket: '
+        # score
+        self.text = [text1+str(currentBestScore), text2+str(currentBestSweets)]
